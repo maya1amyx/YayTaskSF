a5c2c61614234d8145dee34d2274309bf46e75c2
cindylei156 <cindy.lei156@gmail.com>
2018-07-31 15:16:45 -0700

    updates

--- main.py
+++ main.py
@@ -34,20 +34,18 @@ class LoginHandler(webapp2.RequestHandler):
         currentUser = users.get_current_user()
 
         if currentUser:  #if current user exists
-            nickname = currentUser.nickname()  #email address before
 
-            url = users.create_logout_url('/')
-            url_text = "logout"
 
             self.redirect('/PostJobs')
 
         else:
+            
             url = users.create_login_url('/')
             url_text = "login"
 
-        template = jinja_environment.get_template('loginpage.html')
-        self.response.out.write(template.render(url = url,
-        url_text = url_text))
+            template = jinja_environment.get_template('loginpage.html')
+            self.response.out.write(template.render(url = url,
+            url_text = url_text))
         # <a href={{ url}}
 
 
@@ -71,9 +69,10 @@ class Job(ndb.Model):
 class JobPostHandler(webapp2.RequestHandler):
     def get(self):
         #title = self.request.get('jtitle')
+        url = users.create_logout_url('/')
 
         template = jinja_environment.get_template('JobPosting.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(url = url))
 
 
 class JobPostConfirmHandler(webapp2.RequestHandler):
