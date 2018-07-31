ead3f819dfe3387eab656a32adeb9a5a037de770
Maya <maya.amyx@gmail.com>
2018-07-31 15:15:52 -0700

    Working on Find Job page

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.appengine.api import users
 
 template_directory = os.path.join(os.path.dirname(__file__), 'templates') #telling where templates are
 jinja_environment = jinja2.Environment(loader = jinja2.FileSystemLoader(template_directory)) #creating jinja objects
-
+list_of_jobs = []
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         template = jinja_environment.get_template('index.html') #specify which HTML file to serve
@@ -58,6 +58,7 @@ class Job(ndb.Model):
     disc = ndb.StringProperty()
     wage = ndb.StringProperty()
     hours = ndb.StringProperty()
+    job_id = ndb.IntegerProperty()
 
 
 
@@ -76,8 +77,8 @@ class JobPostConfirmHandler(webapp2.RequestHandler):
 
 
     # Creates a category for a job and returns a unique key
-    def create_job_post(self, title, type, disc, wage, hours):
-        post = Job(title=title, type=type, disc=disc, wage= wage, hours=hours)
+    def create_job_post(self, title, type, disc, wage, hours, job_id):
+        post = Job(title=title, type=type, disc=disc, wage= wage, hours=hours, job_id=job_id)
         post = post.put()
     # recieves job info from JobPost page and passes them to the render parameters
     def get(self):
@@ -86,6 +87,7 @@ class JobPostConfirmHandler(webapp2.RequestHandler):
         disc = self.request.get('disc')
         wage = self.request.get('wage')
         hours = self.request.get('hours')
+        job_id = id(self)
 
         #User input is the name of the variable from our aout html file
 
@@ -93,22 +95,25 @@ class JobPostConfirmHandler(webapp2.RequestHandler):
         template = jinja_environment.get_template('JobPostConfirm.html')
         self.response.out.write(template.render(title=title, type=type, disc=disc, wage=wage, hours=hours))
 
-        self.create_job_post(title, type, disc, wage, hours)
+        self.create_job_post(title, type, disc, wage, hours, job_id)
 
 class FindJobsHandler(webapp2.RequestHandler):
     def check_for_empty(self):
         query = Job.query().fetch()
 
         for item in query:
-            if item.title == "u" or item.title=="":
-                item.delete()
+            if item.title == "u":
+                Job.item.delete()
+            elif item.title=="":
+                Job.item.delete()
             else:
                 continue
 
     def get(self):
 
-        list_of_jobs = []
+
         #jobs = []
+        self.check_for_empty()
 
         query = Job.query().fetch(20,keys_only=True)
 
@@ -120,16 +125,23 @@ class FindJobsHandler(webapp2.RequestHandler):
 
         template = jinja_environment.get_template('FindJobs.html')
         self.response.out.write(template.render(query=query, list_of_jobs=list_of_jobs,))
+
 class MoreInfoHandler(webapp2.RequestHandler):
     def get(self):
         # post = post_key.get()
         # return post
+        id = self.request.GET.get("item.job_id")
+        # q = id.all()
+        # q.filter(id="item.job_id")
+        # query = Job.query().fetch(id)
+
 
         template = jinja_environment.get_template('morejobinfo.html')
-        self.response.out.write(template.render())
+        self.response.out.write(template.render(id=id))
+        # self.response.out.write(template.render(id=id, query=query))
 
 
-        
+
 #########################################################################
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
@@ -141,6 +153,7 @@ app = webapp2.WSGIApplication([
     ('/Login', LoginHandler),
     ('/FindJobs', FindJobsHandler),
     ('/MoreInfo', MoreInfoHandler),
+    # ('/MoreInfo/([/w]+)', MoreInfoHandler),
     # ('/SignUp', SignUpHandler),
     ('/JobPostConfirm', JobPostConfirmHandler)
 
